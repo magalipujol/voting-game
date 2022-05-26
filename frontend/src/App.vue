826a652952a2f9db6b7af4f0f238863c0487eb7e
magali <magalipuuujol@gmail.com>
2022-05-26 07:06:30 -0300

    playing with front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 
 import { computed, watchEffect } from 'vue'
-import {useQuery} from '@vue/apollo-composable'
+import { useMutation, useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 
+// create and excecute a query
 const allRoomsQuery = gql`
   query {
     rooms {
@@ -15,20 +16,20 @@ const allRoomsQuery = gql`
     }
   }
 `;
-
 const { result } = useQuery(allRoomsQuery);
 const rooms = computed(() => result.value?.rooms ?? ["error"]);
+
+
 
 </script>
 
 <template>
   <ul>
-    <li v-for="room in rooms" :key="room.id" >
-    {{ room.id }}
+    <li v-for="room in rooms" :key="room.id">
+      {{ room.id }}
     </li>
   </ul>
 </template>
 
 <style>
-
 </style>
